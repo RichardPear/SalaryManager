e4a689bb1c47a33a08b55de86a90d33ec308461e
RichardPear <875860577@qq.com>
2016-03-04 02:18:50 +0800

    delete example file

--- WebRoot/scripts/controller/package/package-index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2015/12/4.
- */
-app.controller("PackageIndexController",['$scope','$rootScope','pengingHttp',function($scope,$rootScope,pengingHttp) {
-    $rootScope.packageURL = './package/package-normal.html';
-    $rootScope.currentModule = '软件包管理';
-
-}]);
